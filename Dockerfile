1b0ece464cccc6707e1bd7463017a8c8bf35a074
Wes Anderson <wesley.herb.anderson@gmail.com>
2026-03-27 08:00:33 -0700

    chore: add minimal HTTP liveness and readiness endpoints and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:1.23-alpine AS build
+
+WORKDIR /src
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+RUN CGO_ENABLED=0 go build -o /prism ./cmd/prism/
+
+FROM scratch
+
+COPY --from=build /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+COPY --from=build /prism /prism
+
+ENTRYPOINT ["/prism"]
--- cmd/prism/main.go
+++ cmd/prism/main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+func main() {
+	defaultPort := 8081
+	if env := os.Getenv("PRISM_HEALTH_PORT"); env != "" {
+		if p, err := strconv.Atoi(env); err == nil && p > 0 {
+			defaultPort = p
+		}
+	}
+
+	healthPort := flag.Int("health-port", defaultPort, "HTTP health check port")
+	flag.Parse()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", *healthPort),
+		Handler: newHealthMux(),
+	}
+
+	go func() {
+		logger.Info("health server starting", "port", *healthPort)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Error("health server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigCh
+
+	logger.Info("shutdown signal received", "signal", sig.String())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error("shutdown", "error", err)
+		return
+	}
+
+	logger.Info("shutdown complete")
+}
